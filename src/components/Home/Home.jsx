171c387489ebecad97ccbc6032b41279959af324
belaeat <omyhossain@gmail.com>
2023-04-11 14:52:06 +0600

    Job Categories added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import JobCategory from '../JobCategory/JobCategory';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const jobCategories = useLoaderData();
+
     return (
         <div className='mx-auto w-[90%] my-12'>
+
+            {/* Banner Section */}
+
             <section className='flex md:flex items-center'>
                 <div className='w-[50%]'>
                     <h1 className='text-6xl font-bold my-8'>One Step 
@@ -13,7 +20,18 @@ const Home = () => {
                     <p className='my-8'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
                     <button className='button-color font-semibold text-white p-3 rounded-lg'>Get Started</button>
                 </div>
-                <img className='w-[50%] h-[50%]' src="../../../public/P3OLGJ1 copy 1.png" alt="" />
+                <img className='w-[50%] h-[50%]' src="/P3OLGJ1 copy 1.png" alt="" />
+            </section>
+
+            {/* Job Category here */}
+            
+            <section className='flex md:flex justify-between items-center my-12'>
+                {
+                    jobCategories.map(category => <JobCategory
+                        key={category.id}
+                        category={category}                        
+                    ></JobCategory>)
+                }
             </section>
         </div>
     );
