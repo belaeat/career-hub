fa6630b6ef9bce427319e9f42c393eecf9370b3b
belaeat <omyhossain@gmail.com>
2023-04-11 06:33:28 +0600

    navbar added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className=' mx-auto w-[90%] flex justify-between items-center mt-5'>
+            <h1 className='text-4xl font-bold'>Job Hunt</h1>
+            <div className='flex gap-6 font-semibold'>
+                <Link to='/'>Home</Link>
+                <Link to='/statistics'>Statistics</Link>
+                <Link to='/applied'>Applied Jobs</Link>
+                <Link to='/blog'>Blog</Link>
+            </div>
+            <button className='button-color font-semibold bg-purple-400 text-white p-3 rounded-lg'>Start Applying</button>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Main from './components/Layout/Main';
+import Statistics from './components/Statistics/Statistics';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Blog from './components/Blog/Blog';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: 'applied',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: 'blog',
+        element: <Blog></Blog>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
