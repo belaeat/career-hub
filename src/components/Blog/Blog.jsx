03d77930870a9bb00b4cf8ab8d5304acc06c2620
belaeat <omyhossain@gmail.com>
2023-04-11 06:45:26 +0600

    blog page done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>This is blog page</h2>
+        <div className='text-center my-12'>
+            <h2 className=' my-12 text-3xl font-bold'>Blog</h2>
+            <div className='p-12 mt-5 bg-green-100 rounded-lg w-[70%] mx-auto'>
+                <h2 className='text-2xl font-bold mb-5'>When should you use context API?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sapiente iusto aperiam dignissimos dolor repudiandae error asperiores placeat delectus velit, architecto, explicabo pariatur sequi consequatur expedita at, quo deserunt deleniti!</p>
+            </div>
+            <div className='p-12 mt-5 bg-green-100 rounded-lg w-[70%] mx-auto'>
+                <h2 className='text-2xl font-bold mb-5'>What is a custom hook?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sapiente iusto aperiam dignissimos dolor repudiandae error asperiores placeat delectus velit, architecto, explicabo pariatur sequi consequatur expedita at, quo deserunt deleniti!</p>
+            </div>
+            <div className='p-12 mt-5 bg-green-100 rounded-lg w-[70%] mx-auto'>
+                <h2 className='text-2xl font-bold mb-5'>What is useRef?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sapiente iusto aperiam dignissimos dolor repudiandae error asperiores placeat delectus velit, architecto, explicabo pariatur sequi consequatur expedita at, quo deserunt deleniti!</p>
+            </div>
+            <div className='p-12 mt-5 bg-green-100 rounded-lg w-[70%] mx-auto'>
+                <h2 className='text-2xl font-bold mb-5'>What is useMemo?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sapiente iusto aperiam dignissimos dolor repudiandae error asperiores placeat delectus velit, architecto, explicabo pariatur sequi consequatur expedita at, quo deserunt deleniti!</p>
+            </div>
         </div>
     );
 };
